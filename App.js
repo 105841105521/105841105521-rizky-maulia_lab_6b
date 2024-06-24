e6bd01a1536c8531040f01bd1a261fccd792a356
105841105521 <105841105521@student.unismuh.ac.id>
2024-06-24 14:43:01 +0800

    costum komponen dan textinput

--- App.js
+++ App.js
@@ -1,53 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
 
-//sign in, sign up, tombol
-const App = () => {
+// Custom component, ButtonCostum, textinput
+const ButtonCostum = ({ text, color }) => {
   return (
-    <View style ={{ flex: 1, flexDirection: "column", justifyContent:"center", alignItems: "center" }}>
-      <View style={ styles.boxBlue}>
-        <Text style={styles.text}>LOGIN</Text>
-      </View>
-
-      <View style={styles.boxGray}>
-        <Text style={styles.text}>REGISTER</Text>
+    <View>
+      <View style={{
+        width: 250,
+        height: 100,
+        backgroundColor: color,
+        borderRadius: 50,
+        marginBottom: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Text style={{
+          textAlign: 'center',
+          color: 'white',
+          fontSize: 30,
+          fontWeight: 'bold',
+        }}>
+          {text}
+        </Text>
       </View>
     </View>
-  );
+  )
 }
 
-//gaya tombol, size, dan jarak
-const styles = StyleSheet.create({
-  boxBlue: {
-    width:130,
-    height: 50,
-    backgroundColor: "#4A90E2",
-    borderRadius: 30,
-    marginBottom: 20, 
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-  },
-  boxGray: {
-    width: 130,
-    height: 50,
-    backgroundColor: "#7f8C8D",
-    borderRadius: 35,
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-  },
-  text: {
-    color: "white",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-})
+// Custom component, TextInputCustom
+const TextInputCustom = ({ placeholder, color, typeKeyboard }) => {
+  return (
+    <TextInput
+      keyboardType={typeKeyboard}
+      placeholder={placeholder}
+      style={{
+        width: 250,
+        height: 50,
+        borderColor: color,
+        borderWidth: 3,
+        borderRadius: 10,
+        marginBottom: 10,
+        paddingLeft: 10,
+      }}
+    />
+  )
+}
 
-export default App;
+export default function App() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <ButtonCostum text="LOGIN" color="blue" />
+      <ButtonCostum text="SIGN IN" color="red" />
+      <ButtonCostum text="REGISTER" color="green" />
+      <TextInputCustom placeholder="Masukkan Nama" color="pink" typeKeyboard="default" />
+      <TextInputCustom placeholder="Masukkan no hp" color="pink" typeKeyboard="numeric" />
+    </View>
+  )
+}
